46ede2f4be5b3ecb8d8994a3c97cf379ebc46ab2
Ванек <ivanguzov822@gmail.com>
2023-08-05 17:52:05 +0300

    Сделана домашка по 8 анятию

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 # Create your models here.
 
 
@@ -20,6 +21,25 @@ class Advertisement(models.Model):
         db_table = 'advertisements'
 
 
+    @admin.display(description= "Дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+
+            created_date = self.created_at.strftime("%H.%M.%S")
+            return format_html('<span style = "color:green; font-weight: bold"> Сегодня в {} </span>', created_date)
+        return self.created_at.strftime("%d.%m.%Y в %H.%M.%S")
+
+
+
+    @admin.display(description= "Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+
+            created_date = self.created_at.strftime("%H.%M.%S")
+            return format_html('<span style = "color:green; font-weight: bold"> Сегодня в {} </span>', created_date)
+        return self.created_at.strftime("%d.%m.%Y в %H.%M.%S")
     def __str__(self):
         return f'Advertisement(id = {self.id}, title = {self.title}, price = {self.price})'
     
