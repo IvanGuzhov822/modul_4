da9a22a039f7f673b381558e899f199b6e38a62c
Ванек <ivanguzov822@gmail.com>
2023-08-23 08:49:37 +0300

    Сделана домашка по 10 лекции

---
+++ advertisements/app_advertisements/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Advertisement
+
+class AdvertisementForm2(ModelForm):
+    
+    title = forms.CharField(max_length=64,
+                             widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control'}))
+    price = forms.DecimalField(widget=forms.NumberInput(attrs={'class' : 'form-control'}))
+    auction = forms.BooleanField(required = False, widget=forms.CheckboxInput(attrs={'class' : 'form-check_input'}))
+    image = forms.ImageField( widget=forms.FileInput(attrs={'class' : 'form-control'}))
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'description', 'price', 'auction', 'image']
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Advertisement
+from .forms import AdvertisementForm2
+from django.urls import reverse
+from django.core.exceptions import ValidationError
+
 
 def index(request):
 
@@ -16,3 +20,28 @@ def page1(request):
 
 def top_sellers(request):
     return render(request , 'top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == 'POST':
+        form = AdvertisementForm2(request.POST, request.FILES)
+        if form.is_valid():
+            adv = Advertisement(**form.cleaned_data)
+            if adv.title[0] == '?':
+                raise ValidationError("Заголовок не может начинаться с вопросительного знака")
+
+
+            else:
+                adv.user = request.user
+                adv.save()
+                url = reverse('main-page')
+                return redirect(url)
+            
+    else:
+        form = AdvertisementForm2()
+
+
+
+
+    context = {'form': form}
+
+    return render(request, 'advertisement-post.html', context)
